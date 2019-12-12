a4ff9602feeb9fa72119e5703ab36cafcc62bde7
Ilya Nixan <thenixan@gmail.com>
2019-12-13 00:03:50 +0300

    Day 12

--- src/task_12.rs
+++ src/task_12.rs
@@ -1,3 +1,4 @@
+extern crate num;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -32,7 +33,6 @@ impl std::str::FromStr for Moon {
         let x = s[0][2..].parse::<i32>().unwrap();
         let y = s[1][2..].parse::<i32>().unwrap();
         let z = s[2][2..].parse::<i32>().unwrap();
-        println!("{:?}", s);
         Ok(Moon {
             x,
             y,
@@ -79,17 +79,43 @@ impl std::iter::FromIterator<String> for PlanetarySystem {
     }
 }
 
-impl std::iter::Iterator for PlanetarySystem {
+struct StateIterator {
+    system: PlanetarySystem,
+}
+
+impl std::iter::Iterator for StateIterator {
+    type Item = Vec<Velocity>;
+    fn next(&mut self) -> Option<Self::Item> {
+        let result = self.system.moons.iter().map(|m| m.v.clone()).collect();
+        self.system.evaluate_velocities();
+        self.system.apply_velocities();
+        Some(result)
+    }
+}
+
+struct EnergyIterator {
+    system: PlanetarySystem,
+}
+
+impl std::iter::Iterator for EnergyIterator {
     type Item = usize;
     fn next(&mut self) -> Option<usize> {
-        let energy = self.moons.iter().map(|m| m.energy()).sum();
-        self.evaluate_velocities();
-        self.apply_velocities();
+        let energy = self.system.moons.iter().map(|m| m.energy()).sum();
+        self.system.evaluate_velocities();
+        self.system.apply_velocities();
         Some(energy)
     }
 }
 
 impl PlanetarySystem {
+    fn energy_iterator(self) -> EnergyIterator {
+        EnergyIterator { system: self }
+    }
+
+    fn state_iterator(self) -> StateIterator {
+        StateIterator { system: self }
+    }
+
     fn evaluate_velocities(&mut self) {
         for i in 0..self.moons.len() {
             let moon = &self.moons[i];
@@ -133,11 +159,41 @@ pub fn run() {
         .filter_map(|l| l.ok())
         .collect::<PlanetarySystem>();
 
-    let result = system.take(1001).last().unwrap();
+    let result = system.energy_iterator().take(1001).last().unwrap();
     println!("Result: {}", result);
 }
 
 pub fn run_e() {
-    let input = File::open("input/task_11").unwrap();
-    let mut input = BufReader::new(input);
+    let input = File::open("input/task_12").unwrap();
+    let input = BufReader::new(input);
+    let system = input
+        .lines()
+        .filter_map(|l| l.ok())
+        .collect::<PlanetarySystem>();
+
+    let mut has_x = false;
+    let mut has_y = false;
+    let mut has_z = false;
+    let result = system
+        .state_iterator()
+        .enumerate()
+        .skip(1)
+        .filter_map(|(i, m)| {
+            if !has_x && m.iter().map(|v| v.x).all(|x| x == 0) {
+                has_x = true;
+                Some(i)
+            } else if !has_y && m.iter().map(|v| v.y).all(|y| y == 0) {
+                has_y = true;
+                Some(i)
+            } else if !has_z && m.iter().map(|v| v.z).all(|z| z == 0) {
+                has_z = true;
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .take(3)
+        .fold(1, |acc, i| num::integer::lcm(acc, i))
+        * 2;
+    println!("Result: {}", result);
 }
